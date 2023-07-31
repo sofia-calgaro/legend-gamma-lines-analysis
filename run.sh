2e261198c2c2014c75ec9996493b8dafdfafaa86
Valentina Biancacci <valentina.biancacci@pd.infn.it>
2023-07-31 11:06:06 +0200

    check on qsub

--- run.sh
+++ run.sh
@@ -2,20 +2,25 @@
 
 detector_type="$(grep -oP '(?<="detectors": ")[^"]*' $1)"
 if [ "$detector_type" == "single" ]; then
-   detector_list=$(cat ./list_detectors_p3p4.json | jq -r '.[]')
-   for d in $detector_list
+   file="./list_detectors_p3p4.json"
+   detector_list=$(python-3.9.6 -c \
+"import json;
+with open('$file', 'r') as f:
+  print(json.load(f)['detector_p3p4'])")
+  for d in $detector_list
    do
-    if [ "$d" == ']' ] || [ "$d" == '[' ]; then
-       continue
-    fi
-    d_name=$(echo $d| sed 's/"//g')
-    d_name=$(echo $d_name| sed 's/,//g')
-    JobName="$d""_bat"
-    #qsub -N $JobName run-bat.qsub $1 $d_name $d
+    d_name=$(echo "${d//\'}")
+    d_name=$(echo "${d_name//\[}")
+    d_name=$(echo "${d_name//\]}")
+    d_name=$(echo "${d_name//\,}")
+    JobName="$d_name""_bat"
+    echo $d_name
+    echo $d
+    #./run_test.sh $1 $detector_type $d_name
+    qsub -N $JobName run-bat.qsub $1 $detector_type $d_name
     done
-else
+   else
     JobName="$detector""_bat"
-    #qsub -N $JobName run-bat.qsub $1 $detector;
+    #qsub -N $JobName run-bat.qsub $1 $detector_type;
 fi
-
 
